0cdd9af80ccb2faa7bccb3535fe88019f2b20dcd
zsalwen <zach@mdwestserve.com>
2010-10-13 08:20:38 -0700

    Removing while loop, only displaying instruction to serve occupant(s) at address.

--- ev_instructions.php
+++ ev_instructions.php
@@ -12,6 +12,7 @@ $deadline=$deadline+432000;
 $estFileDate=strtotime($data[estFileDate]);
 $estFileDate=date('m/d/Y',$estFileDate);
 $server_notes=$data[server_notes];
+$add1x = $data["address1"].' '.$data["city1"].', '.$data["state1"].' '.$data["zip1"];
 ?>
 <style>body { margin:0px; padding:0px;}</style>
 <img style="position:absolute; left:0px; top:0px; width:100px; height:100px;" src="small.logo.gif" class="logo">
@@ -22,23 +23,14 @@ $server_notes=$data[server_notes];
 	<tr>
 		<td align="center" style="font-size:22px; font-variant:small-caps;">Received: <?=$received?> || Affidavit Deadline: <?=$estFileDate?><br>This page must be returned with affidavits for payment of service.</td>
 	</tr>
-<?
-$add1x = $data["address1"].' '.$data["city1"].', '.$data["state1"].' '.$data["zip1"];
-$i=0;
-while ($i < 6){$i++;
-	if ($data["name$i"]){
-		$name=ucwords($data["name$i"]);
-		?>
 		<tr>
 		<td>
-		<strong><?=$name?>:</strong>
-		<ol><li><?=id2name($data[server_id])?> is to make 2 service attempts on <?=$name?> at <?=$add1x?> on different days.</li><li>
+		<strong>OCCUPANT:</strong>
+		<ol><li><?=id2name($data[server_id])?> is to make 2 service attempts on the occupant(s) of <?=$add1x?> on different days.</li><li>
 		After all other attempts have proven unsuccessful, 
-		If <?=id2name($data[server_id])?> is unable to serve <?=$name?>:<br />
+		If <?=id2name($data[server_id])?> is unable to serve an occupant of suitable age and discretion:<br />
 		<?=id2name($data[server_id])?> is to post <?=$add1x?>.</li></ol>
 		</ol></td></tr>
-<?	}
-} ?>
 		<tr>
 			<td align='center'><fieldset><legend>Staff Instructions to Server</legend><li>In The Event Of Posting, Write The Date And Time Of Posting On The Documents Being Left. Please Ensure That This Date Is Also Visible In The Posting Picture.</li><li><b>Make one attempt either before 8 AM or after 6 PM, and another attempt between 9 AM and 5 PM.  "Good Faith" efforts must be made at different times of day.</b></li><li><b>Delivery to MDWestServe of all service affidavits for this file must be accomplished before <?=$estFileDate?></b></li><?=strtoupper($server_notes);?></fieldset></td>
 		</tr>
