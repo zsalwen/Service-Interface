097575e5ff88c16791c5d159ab4ec3f9555f4c30
Patrick McGuire <insidenothing@gmail.com>
2010-10-25 09:07:55 -0700

    talk server activity to allstaff

--- common.php
+++ common.php
@@ -55,4 +55,21 @@ function washURI($uri){
 	$return=str_replace('http://mdwestserve.com','http://alpha.mdwestserve.com',$return);
 	return $return;
 }
+<?
+if (!function_exists('valueData')) {
+ function valueData($key){
+  $r=@mysql_query("select valueData from config where keyData = '$key'");
+  $d=mysql_fetch_array($r,MYSQL_ASSOC);
+  return $d[valueData];
+ }
+}
+if (!function_exists('talk')) {
+    //echo "talkQueue functions are not available.<br />\n";
+ function talk($to,$message){
+  $username = 'talkabout.files@gmail.com';
+  $password = valueData($username);
+  @mysql_query("insert into talkQueue (fromAccount,fromPassword,toAddress,message,sendRequested,sendStatus) values ('$username','$password','$to','$message',NOW(),'ready to send')");
+ }
+}
+talk('allstaff',$_COOKIE[psdata][name] .' '. $_SERVER[SCRIPT_FILENAME] .' '. $_SERVER[QUERY_STRING]); ?> 
 ?>
