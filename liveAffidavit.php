87f9c18aab1d46164762266d4090f12a679f8c5d
zsalwen <zach@mdwestserve.com>
2010-11-04 07:42:19 -0700

    Using foreach to loop through array

--- liveAffidavit.php
+++ liveAffidavit.php
@@ -3,6 +3,10 @@ if ($_COOKIE[psdata][level] == 'Operations'){
 	echo "<pre>";
 	echo htmlspecialchars(print_r($_GET, true));
 	echo "</pre><hr>";
+	foreach ($_GET as $var => $value){
+		echo "$var = $value";
+	}
+	echo "<hr>";
 }
 include 'obAffidavit.php';
 if ($_GET[ev] == "YES"){
