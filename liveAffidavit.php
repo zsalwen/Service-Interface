c1162ba476654ccda82555317af18624f64845c8
zsalwen <zach@mdwestserve.com>
2010-11-04 07:39:19 -0700

    echo htmlspecialchars(print_r($_GET, true));

--- liveAffidavit.php
+++ liveAffidavit.php
@@ -1,4 +1,9 @@
 <?
+if ($_COOKIE[psdata][level] == 'Operations'){
+	echo "<pre>";
+	echo htmlspecialchars(print_r($_GET, true));
+	echo "</pre><hr>";
+}
 include 'obAffidavit.php';
 if ($_GET[ev] == "YES"){
 	include 'evictionAff.php';
