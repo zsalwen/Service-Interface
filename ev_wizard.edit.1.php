b5aae8f78c621c85639b4203cc201f920d11fa37
zsalwen <zach@mdwestserve.com>
2010-11-23 06:06:47 -0800

    Setting addressType manually, limiting address dropdowns to address1 (since a-e are unused)

--- ev_wizard.edit.1.php
+++ ev_wizard.edit.1.php
@@ -35,7 +35,7 @@ foreach ($_POST[remove] as $key => $value){
 	if ($d[action_type] == 'Attempted Service' || $d[action_type] == 'Posted Papers'){$items++;
 		//1. address
 		$address=$d[address];
-		$addressType=getAddressType($address);
+		$addressType="RESIDENTIAL PROPERTY SUBJECT TO MORTGAGE OR DEED OF TRUST";
 		//2. month, day, year dropdowns
 		//3. hour, minute am/pm
 		//4. explode desc
@@ -51,21 +51,6 @@ foreach ($_POST[remove] as $key => $value){
 		<select name="address_source-<?=$key?>">
 			<option><?=$address?></option>
 			<option value="1"><?=$ddr[address1]?>, <?=$ddr[city1]?>, <?=$ddr[state1]?> <?=$ddr[zip1]?></option>
-			<? if ($ddr[address1a]){?>
-			<option value="1a"><?=$ddr[address1a]?>, <?=$ddr[city1a]?>, <?=$ddr[state1a]?> <?=$ddr[zip1a]?></option>
-			<? }
-			if ($ddr[address1b]){?>
-			<option value="1b"><?=$ddr[address1b]?>, <?=$ddr[city1b]?>, <?=$ddr[state1b]?> <?=$ddr[zip1b]?></option>
-			<? }
-			if ($ddr[address1c]){?>
-			<option value="1c"><?=$ddr[address1c]?>, <?=$ddr[city1c]?>, <?=$ddr[state1c]?> <?=$ddr[zip1c]?></option>
-			<? } 
-			if ($ddr[address1d]){?>
-			<option value="1d"><?=$ddr[address1d]?>, <?=$ddr[city1d]?>, <?=$ddr[state1d]?> <?=$ddr[zip1d]?></option>
-			<? } 
-			if ($ddr[address1e]){?>
-			<option value="1e"><?=$ddr[address1e]?>, <?=$ddr[city1e]?>, <?=$ddr[state1e]?> <?=$ddr[zip1e]?></option>
-			<? } ?>
 		</select>
 		</td></tr>
 		<tr><td colspan=2>2) Month, Day, Year <select name="month-<?=$key?>"><?=mkmonth($month)?></select> <select name="day-<?=$key?>"><?=mkday($day)?></select> <select name="year-<?=$key?>"><?=mkyear($year)?></select></td></tr>
@@ -79,7 +64,7 @@ foreach ($_POST[remove] as $key => $value){
 	}elseif ($d[wizard] == 'NOT BORROWER'){$items++;
 		//1. address
 		$address=$d[address];
-		$addressType=getAddressType($address);
+		$addressType="RESIDENTIAL PROPERTY SUBJECT TO MORTGAGE OR DEED OF TRUST";
 		//2. resident name
 		$name=$d[resident];
 		//3. resident age
@@ -94,21 +79,6 @@ foreach ($_POST[remove] as $key => $value){
 		<select name="address_source-<?=$key?>">
 			<option><?=$address?></option>
 			<option value="1"><?=$ddr[address1]?>, <?=$ddr[city1]?>, <?=$ddr[state1]?> <?=$ddr[zip1]?></option>
-			<? if ($ddr[address1a]){?>
-			<option value="1a"><?=$ddr[address1a]?>, <?=$ddr[city1a]?>, <?=$ddr[state1a]?> <?=$ddr[zip1a]?></option>
-			<? }
-			if ($ddr[address1b]){?>
-			<option value="1b"><?=$ddr[address1b]?>, <?=$ddr[city1b]?>, <?=$ddr[state1b]?> <?=$ddr[zip1b]?></option>
-			<? }
-			if ($ddr[address1c]){?>
-			<option value="1c"><?=$ddr[address1c]?>, <?=$ddr[city1c]?>, <?=$ddr[state1c]?> <?=$ddr[zip1c]?></option>
-			<? } 
-			if ($ddr[address1d]){?>
-			<option value="1d"><?=$ddr[address1d]?>, <?=$ddr[city1d]?>, <?=$ddr[state1d]?> <?=$ddr[zip1d]?></option>
-			<? } 
-			if ($ddr[address1e]){?>
-			<option value="1e"><?=$ddr[address1e]?>, <?=$ddr[city1e]?>, <?=$ddr[state1e]?> <?=$ddr[zip1e]?></option>
-			<? } ?>
 		</select>
 		</td></tr>
 		<tr><td>2) Name</td><td><input name="name" value="<?=$name;?>" size="20"></td></tr>
@@ -121,7 +91,7 @@ foreach ($_POST[remove] as $key => $value){
 	}elseif($d[wizard] == 'BORROWER'){$items++;
 		//1. address
 		$address=$d[address];
-		$addressType=getAddressType($address);
+		$addressType="RESIDENTIAL PROPERTY SUBJECT TO MORTGAGE OR DEED OF TRUST";
 		//2. month, day, year dropdowns
 		//3. hour, minute am/pm
 		//4. residentDesc
@@ -131,22 +101,6 @@ foreach ($_POST[remove] as $key => $value){
 		<table width="20%"><tr><td colspan=2>1) Select Address 
 		<select name="address_source-<?=$key?>">
 			<option><?=$address?></option>
-			<option value="<?=$ddr[address1]?>, <?=$ddr[city1]?>, <?=$ddr[state1]?> <?=$ddr[zip1]?>"><?=$ddr[address1]?>, <?=$ddr[city1]?>, <?=$ddr[state1]?> <?=$ddr[zip1]?></option>
-			<? if ($ddr[address1a]){?>
-			<option value="<?=$ddr[address1]?>, <?=$ddr[city1]?>, <?=$ddr[state1]?> <?=$ddr[zip1]?>"><?=$ddr[address1a]?>, <?=$ddr[city1a]?>, <?=$ddr[state1a]?> <?=$ddr[zip1a]?></option>
-			<? }
-			if ($ddr[address1b]){?>
-			<option value="<?=$ddr[address1]?>, <?=$ddr[city1]?>, <?=$ddr[state1]?> <?=$ddr[zip1]?>"><?=$ddr[address1b]?>, <?=$ddr[city1b]?>, <?=$ddr[state1b]?> <?=$ddr[zip1b]?></option>
-			<? }
-			if ($ddr[address1c]){?>
-			<option value="<?=$ddr[address1]?>, <?=$ddr[city1]?>, <?=$ddr[state1]?> <?=$ddr[zip1]?>"><?=$ddr[address1c]?>, <?=$ddr[city1c]?>, <?=$ddr[state1c]?> <?=$ddr[zip1c]?></option>
-			<? } 
-			if ($ddr[address1d]){?>
-			<option value="<?=$ddr[address1]?>, <?=$ddr[city1]?>, <?=$ddr[state1]?> <?=$ddr[zip1]?>"><?=$ddr[address1d]?>, <?=$ddr[city1d]?>, <?=$ddr[state1d]?> <?=$ddr[zip1d]?></option>
-			<? } 
-			if ($ddr[address1e]){?>
-			<option value="1e"><?=$ddr[address1e]?>, <?=$ddr[city1e]?>, <?=$ddr[state1e]?> <?=$ddr[zip1e]?></option>
-			<? } ?>
 		</select>
 		</td></tr>
 		<tr><td colspan=2>2) Month, Day, Year <select name="month-<?=$key?>"><?=mkmonth($month)?></select> <select name="day-<?=$key?>"><?=mkday($day)?></select> <select name="year-<?=$key?>"><?=mkyear($year)?></select></td></tr>
@@ -173,11 +127,6 @@ foreach ($_POST[remove] as $key => $value){
 	<input type="hidden" name="served-<?=$key?>" value="<?=$d[wizard]?>" />
 	<input type="hidden" name="addressType-<?=$key?>" value="<?=$addressType?>" />
 	<input type="hidden" name="address1-<?=$key?>" value="<?=$d[address1]?>, <?=$d[city1]?>, <?=$d[state1]?> <?=$d[zip1]?>" />
-	<input type="hidden" name="address1a-<?=$key?>" value="<?=$d[address1a]?>, <?=$d[city1a]?>, <?=$d[state1a]?> <?=$d[zip1a]?>" />
-	<input type="hidden" name="address1b-<?=$key?>" value="<?=$d[address1b]?>, <?=$d[city1b]?>, <?=$d[state1b]?> <?=$d[zip1b]?>" />
-	<input type="hidden" name="address1c-<?=$key?>" value="<?=$d[address1c]?>, <?=$d[city1c]?>, <?=$d[state1c]?> <?=$d[zip1c]?>" />
-	<input type="hidden" name="address1d-<?=$key?>" value="<?=$d[address1d]?>, <?=$d[city1d]?>, <?=$d[state1d]?> <?=$d[zip1d]?>" />
-	<input type="hidden" name="address1e-<?=$key?>" value="<?=$d[address1e]?>, <?=$d[city1e]?>, <?=$d[state1e]?> <?=$d[zip1e]?>" />
 	<input type="hidden" name="remove[<?=$key?>]"/>
 	<?
 	//display editing inputs
