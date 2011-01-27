43a0134a66b0f64941fc8fda6bfbf8a25a1cd66b
zsalwen <zach@mdwestserve.com>
2011-01-27 13:28:25 -0500

    Beginning folder with "EV"

--- ev_wizard.photo.upload.2.php
+++ ev_wizard.photo.upload.2.php
@@ -20,22 +20,20 @@ if ($_FILES['upload']['tmp_name']){
 @mysql_query("insert into ps_file_array (name, type, size, tmp_name, error, uploadDate, user) values ('".$_FILES['upload']['name']."','".$_FILES['upload']['type']."','".$_FILES['upload']['size']."','".$_FILES['upload']['tmp_name']."','".$_FILES['upload']['error']."', NOW(), '$name' )");
 // ok first we need to go get the files
 $path = "/data/service/photos/";
-$file_path = $path.'/'.$packet;
+$file_path = $path.'/EV'.$packet;
 if (!file_exists($file_path)){
 	mkdir ($file_path,0777);
 }
 $target_path = $file_path."/".$defendant.".".$_POST[photo].".".time().".jpg";  
 if (move_uploaded_file($_FILES['upload']['tmp_name'], $target_path)){"file <b>NOT</b> saved...($target_path)<br>"; }else{ echo "file saved...($target_path)<br>"; }
 
-$link = "http://mdwestserve.com/ps/photographs/".$packet."/".$defendant.".".$_POST[photo].".".time().".jpg";
+$link = "http://service.mdwestserve.com/photographs/EV".$packet."/".$defendant.".".$_POST[photo].".".time().".jpg";
 $query = "UPDATE evictionPackets SET photo".$defendant.$_POST[photo]." ='$link', photoStatus='PHOTO UPLOADED' where eviction_id = '$packet'";
 $user = $_COOKIE[psdata][user_id];
 $addressID=alpha2ID($_POST[photo]);
 $query2 = "INSERT into ps_photos (packetID,defendantID,addressID,serverID,localPath,browserAddress) VALUES ('EV$packet','$defendant','$addressID','$user','$target_path','$link')";
 @mysql_query($query);
 @mysql_query($query2);
-// do all watermarking here!
-include 'wizard.photo.watermark.php';
 // send html with img tags....
 $headers  = "MIME-Version: 1.0 \n";
 $headers .= "Content-type: text/html; charset=iso-8859-1 \n";
