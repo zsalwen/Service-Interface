81f8cafa48e22cfb895b45635fe7067759513528
zsalwen <zach@mdwestserve.com>
2011-02-01 11:08:17 -0500

    Adding photojump option

--- ev_wizard.jump.php
+++ ev_wizard.jump.php
@@ -1,5 +1,9 @@
 <body onLoad="document.form1.submit()">
+<? if ($_GET[photojump]){?>
+<form method="post" name="form1" id="form1" action="ev_wizard.php" target="_self"><input type="hidden" name="i" value="photo.upload.1" /><input type="hidden" name="parts" value="<?=$_GET[jump]?>" /><input type="hidden" name="opServer" value="<?=$_GET[server]?>" />
+<? }else{ ?>
 <form method="post" name="form1" id="form1" action="ev_wizard.php" target="_self"><input type="hidden" name="i" value="1" /><input type="hidden" name="parts" value="<?=$_GET[jump]?>" /><input type="hidden" name="opServer" value="<?=$_GET[server]?>" />
+<? } ?>
 <? if ($_POST[mailDate]){  ?>
 <input type="hidden" name="mailDate" value="<?=$_POST[mailDate]?>">
 <? } ?>
