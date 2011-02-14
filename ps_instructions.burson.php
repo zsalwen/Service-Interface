646a429c454d47b344f58a12e2a780562662134b
zsalwen <zach@mdwestserve.com>
2011-02-14 14:28:03 -0500

    <b> instead of <strong>

--- ps_instructions.burson.php
+++ ps_instructions.burson.php
@@ -164,7 +164,7 @@ if ($data[lossMit] != '' && $data[lossMit] != 'N/A - OLD L' && $data[packet_id] 
     	<td colspan="6" align="center"><b>Service Type 'B':</b></td>
     </tr>
     <tr>
-    	<td colspan="6" align="center"><div style="width:650px; text-align:left;"><div style="border:ridge 3px; font-size: 16px;"><strong style="text-decoration:underline;">ALL SERVICE MUST TAKE INTO ACCOUNT:</strong><br>'Two good faith attempts on separate days' - MD Rule 14-209(b)</div><ul><li>You cannot make two attempts at the same property within the same 24 hour period.</li>
+    	<td colspan="6" align="center"><div style="width:650px; text-align:left;"><div style="border:ridge 3px; font-size: 16px;"><b style="text-decoration:underline;">ALL SERVICE MUST TAKE INTO ACCOUNT:</b><br>'Two good faith attempts on separate days' - MD Rule 14-209(b)</div><ul><li>You cannot make two attempts at the same property within the same 24 hour period.</li>
         <li><b>posting</b> must be done <b>after</b> <u>all attempts</u> have been completed.</li>
         <li>Attempts are to be performed in order listed on service instructions.</li>
 		<li><b>Make one attempt either before 8 AM or after 6 PM, and another attempt between 9AM and 5PM.  Please avoid attempts between 8AM-9AM & 5PM-6PM.  "Good Faith" efforts must be made at different times of day, meaning that one attempt should be made <b>before</b> noon, and another afterwards, and they must be at least 24 hours apart.</b></li>
