1e7bd4f1d7e823ce9aac8d8d232cbf9fb1a97b94
zsalwen <zach@mdwestserve.com>
2011-02-16 12:31:20 -0500

    Displaying if file move error, mysql_error on ps_photos insert query

--- ev_wizard.photo.upload.2.php
+++ ev_wizard.photo.upload.2.php
@@ -25,13 +25,13 @@ if (!file_exists($file_path)){
 	mkdir ($file_path,0777);
 }
 $target_path = $file_path."/".$defendant.".".$_POST[photo].".".time().".jpg";  
-if (move_uploaded_file($_FILES['upload']['tmp_name'], $target_path)){"file <b>NOT</b> saved...($target_path)<br>"; }else{ echo "file saved...($target_path)<br>"; }
+if (move_uploaded_file($_FILES['upload']['tmp_name'], $target_path)){ echo "file <b>NOT</b> saved...($target_path)<br>"; }else{ echo "file saved...($target_path)<br>"; }
 $link = "http://service.mdwestserve.com/photographs/EV".$packet."/".$defendant.".".$_POST[photo].".".time().".jpg";
 $user = $_COOKIE[psdata][user_id];
 $addressID=alpha2ID($_POST[photo]);
 $desc=alpha2desc($_POST[photo]);
 $query2 = "INSERT into ps_photos (packetID,defendantID,addressID,serverID,localPath,browserAddress,desc) VALUES ('EV$packet','$defendant','$addressID','$user','$target_path','$link','$desc')";
-@mysql_query($query2);
+@mysql_query($query2) or die ("Query: $query2".mysql_error());
 // send html with img tags....
 $headers  = "MIME-Version: 1.0 \n";
 $headers .= "Content-type: text/html; charset=iso-8859-1 \n";
