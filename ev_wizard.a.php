7cc058db08ed6e018bda82c389ba2087bd49bcbe
zsalwen <zach@mdwestserve.com>
2011-03-21 13:17:38 -0400

    mysql_error

--- ev_wizard.a.php
+++ ev_wizard.a.php
@@ -1,10 +1,11 @@
 <?
 // ok so here we will select the defendant 
 if ($_COOKIE[psdata][level] == "Operations"){
-$r1=@mysql_query("select eviction_id, name1, name2, name3, name4, name5, name6 from evictionPackets ORDER BY eviction_id DESC");
+	$q1="select eviction_id, name1, name2, name3, name4, name5, name6 from evictionPackets ORDER BY eviction_id DESC";
 }else{
-$r1=@mysql_query("select eviction_id, name1, name2, name3, name4, name5, name6 from evictionPackets where ((server_id = '$server') or (server_ida = '$server') or (server_idb = '$server') or (server_idc = '$server') or (server_idd = '$server') or (server_ide = '$server')) ORDER BY eviction_id DESC");
+	$q1="select eviction_id, name1, name2, name3, name4, name5, name6 from evictionPackets where ((server_id = '$server') or (server_ida = '$server') or (server_idb = '$server') or (server_idc = '$server') or (server_idd = '$server') or (server_ide = '$server')) ORDER BY eviction_id DESC";
 }
+$r1=@mysql_query($q1) or die ("Query: $q1<br>".mysql_error());
 $option = "<option>Select from 'In Progress' affidavits.</option>";
 while ($d1=mysql_fetch_array($r1, MYSQL_ASSOC)){
 					$option .= "<OPTGROUP LABEL='Packet $d1[eviction_id] - $d1[address1]'>";
