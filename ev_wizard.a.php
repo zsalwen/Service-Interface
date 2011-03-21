408196488fa6e50de8b6715ab65f7638f1433fd0
zsalwen <zach@mdwestserve.com>
2011-03-21 17:08:53 -0400

    if $r1, then loop through $d1

--- ev_wizard.a.php
+++ ev_wizard.a.php
@@ -1,21 +1,23 @@
 <?
 // ok so here we will select the defendant 
 if ($_COOKIE[psdata][level] == "Operations"){
-	$q1="select eviction_id, name1, name2, name3, name4, name5, name6 from evictionPackets ORDER BY eviction_id DESC";
+	$q1="select eviction_id, address1, name1, name2, name3, name4, name5, name6 from evictionPackets ORDER BY eviction_id DESC";
 }else{
-	$q1="select eviction_id, name1, name2, name3, name4, name5, name6 from evictionPackets where ((server_id = '$server') or (server_ida = '$server') or (server_idb = '$server') or (server_idc = '$server') or (server_idd = '$server') or (server_ide = '$server')) ORDER BY eviction_id DESC";
+	$q1="select eviction_id, address1, name1, name2, name3, name4, name5, name6 from evictionPackets where ((server_id = '$server') or (server_ida = '$server') or (server_idb = '$server') or (server_idc = '$server') or (server_idd = '$server') or (server_ide = '$server')) ORDER BY eviction_id DESC";
 }
 $r1=@mysql_query($q1) or die ("Query: $q1<br>".mysql_error());
 $option = "<option>Select from 'In Progress' affidavits.</option>";
-while ($d1=mysql_fetch_array($r1, MYSQL_ASSOC)){
-					$option .= "<OPTGROUP LABEL='Packet $d1[eviction_id] - $d1[address1]'>";
-					$option .= "<option value='$d1[eviction_id]-1'>$d1[name1]</option>";
-if ($d1[name2]){	$option .= "<option value='$d1[eviction_id]-2'>$d1[name2]</option>"; }
-if ($d1[name3]){	$option .= "<option value='$d1[eviction_id]-3'>$d1[name3]</option>"; }
-if ($d1[name4]){	$option .= "<option value='$d1[eviction_id]-4'>$d1[name4]</option>"; }
-if ($d1[name5]){	$option .= "<option value='$d1[eviction_id]-4'>$d1[name5]</option>"; }
-if ($d1[name6]){	$option .= "<option value='$d1[eviction_id]-4'>$d1[name6]</option>"; }
-					$option .= "</OPTGROUP>";
+if ($r1){
+	while ($d1=mysql_fetch_array($r1, MYSQL_ASSOC)){
+						$option .= "<OPTGROUP LABEL='Packet $d1[eviction_id] - $d1[address1]'>";
+						$option .= "<option value='$d1[eviction_id]-1'>$d1[name1]</option>";
+	if ($d1[name2]){	$option .= "<option value='$d1[eviction_id]-2'>$d1[name2]</option>"; }
+	if ($d1[name3]){	$option .= "<option value='$d1[eviction_id]-3'>$d1[name3]</option>"; }
+	if ($d1[name4]){	$option .= "<option value='$d1[eviction_id]-4'>$d1[name4]</option>"; }
+	if ($d1[name5]){	$option .= "<option value='$d1[eviction_id]-4'>$d1[name5]</option>"; }
+	if ($d1[name6]){	$option .= "<option value='$d1[eviction_id]-4'>$d1[name6]</option>"; }
+						$option .= "</OPTGROUP>";
+	}
 }
 ?>
 <input type="hidden" name="i" value="1" />
--- wizard.a.php
+++ wizard.a.php
@@ -1,18 +1,22 @@
 <?
 // ok so here we will select the defendant 
 if ($_COOKIE[psdata][level] == "Operations"){
-$r1=@mysql_query("select * from ps_packets ORDER BY packet_id DESC");
+$r1=@mysql_query("select address1, packet_id, name1, name2, name3, name4, name5, name6 from ps_packets ORDER BY packet_id DESC");
 }else{
-$r1=@mysql_query("select * from ps_packets where ((server_id = '$server') or (server_ida = '$server') or (server_idb = '$server') or (server_idc = '$server') or (server_idd = '$server') or (server_ide = '$server')) ORDER BY packet_id DESC");
+$r1=@mysql_query("select address1, packet_id, name1, name2, name3, name4, name5, name6 from ps_packets where ((server_id = '$server') or (server_ida = '$server') or (server_idb = '$server') or (server_idc = '$server') or (server_idd = '$server') or (server_ide = '$server')) ORDER BY packet_id DESC");
 }
 $option = "<option>Select from 'In Progress' affidavits.</option>";
-while ($d1=mysql_fetch_array($r1, MYSQL_ASSOC)){
-					$option .= "<OPTGROUP LABEL='Packet $d1[packet_id] - $d1[address1]'>";
-					$option .= "<option value='$d1[packet_id]-1'>$d1[name1]</option>";
-if ($d1[name2]){	$option .= "<option value='$d1[packet_id]-2'>$d1[name2]</option>"; }
-if ($d1[name3]){	$option .= "<option value='$d1[packet_id]-3'>$d1[name3]</option>"; }
-if ($d1[name4]){	$option .= "<option value='$d1[packet_id]-4'>$d1[name4]</option>"; }
-					$option .= "</OPTGROUP>";
+if ($r1){
+	while ($d1=mysql_fetch_array($r1, MYSQL_ASSOC)){
+						$option .= "<OPTGROUP LABEL='Packet $d1[packet_id] - $d1[address1]'>";
+						$option .= "<option value='$d1[packet_id]-1'>$d1[name1]</option>";
+	if ($d1[name2]){	$option .= "<option value='$d1[packet_id]-2'>$d1[name2]</option>"; }
+	if ($d1[name3]){	$option .= "<option value='$d1[packet_id]-3'>$d1[name3]</option>"; }
+	if ($d1[name4]){	$option .= "<option value='$d1[packet_id]-4'>$d1[name4]</option>"; }
+	if ($d1[name5]){	$option .= "<option value='$d1[packet_id]-4'>$d1[name5]</option>"; }
+	if ($d1[name6]){	$option .= "<option value='$d1[packet_id]-4'>$d1[name6]</option>"; }
+						$option .= "</OPTGROUP>";
+	}
 }
 ?>
 <input type="hidden" name="i" value="1" />
