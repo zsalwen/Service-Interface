6ba89bae963032390aa02d272ea3bd2f62cdd17f
zsalwen <zach@mdwestserve.com>
2011-05-02 10:13:41 -0400

    listing punch_date

--- staffStatus.php
+++ staffStatus.php
@@ -3,21 +3,21 @@ include 'functions.php';
 mysql_connect();
 function lastPunch($id){
 mysql_select_db('core');
-$r=@mysql_query("select user_id, punch_time, action from MDWestServeTimeClock where user_id = '$id' order by punch_id desc LIMIT 0,1");
+$r=@mysql_query("select user_id, punch_time, punch_date, action from MDWestServeTimeClock where user_id = '$id' order by punch_id desc LIMIT 0,1");
 $d=mysql_fetch_array($r, MYSQL_ASSOC);
-return "$d[punch_time] ".id2name($id)." $d[action]<br>";
+return "$d[punch_time] $d[punch_date] ".id2name($id)." $d[action]<br>";
 }
 function lastPunch2($id){
 mysql_select_db('intranet');
-$r=@mysql_query("select user_id, punch_time, action from timeclock where user_id = '$id' order by punch_id desc LIMIT 0,1");
+$r=@mysql_query("select user_id, punch_time, punch_date, action from timeclock where user_id = '$id' order by punch_id desc LIMIT 0,1");
 $d=mysql_fetch_array($r, MYSQL_ASSOC);
-return "$d[punch_time] ".id2name2($id)." $d[action]<br>";
+return "$d[punch_time] $d[punch_date] ".id2name2($id)." $d[action]<br>";
 }
 mysql_select_db('core');
 $r=@mysql_query("SELECT distinct user_id from MDWestServeTimeClock order by user_id");
 while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
 	if ($d[user_id] == 1 || $d[user_id] == 2 || $d[user_id] == 265 || $d[user_id] == 296 || $d[user_id] == 308 || $d[user_id] == 370){
-		echo "<li style='background-color:yellow;'>".lastPunch($d[user_id])."</li>";
+		echo "<li style='background-color:yellow; width=500px;'>".lastPunch($d[user_id])."</li>";
 	}else{
 		echo lastPunch($d[user_id]);
 	}
