0e30b2351c937d22f5b411185d795a561206b97b
zsalwen <zach@mdwestserve.com>
2011-05-02 10:37:31 -0400

    dynamically highlighting all Operations-level users

--- staffStatus.php
+++ staffStatus.php
@@ -2,21 +2,27 @@
 include 'functions.php';
 mysql_connect();
 function lastPunch($id){
-mysql_select_db('core');
-$r=@mysql_query("select user_id, punch_time, punch_date, action from MDWestServeTimeClock where user_id = '$id' order by punch_id desc LIMIT 0,1");
-$d=mysql_fetch_array($r, MYSQL_ASSOC);
-return "$d[punch_time] $d[punch_date] ".id2name($id)." $d[action]<br>";
+	mysql_select_db('core');
+	$r=@mysql_query("select user_id, punch_time, punch_date, action from MDWestServeTimeClock where user_id = '$id' order by punch_id desc LIMIT 0,1");
+	$d=mysql_fetch_array($r, MYSQL_ASSOC);
+	return "$d[punch_time] $d[punch_date] ".id2name($id)." $d[action]<br>";
 }
 function lastPunch2($id){
-mysql_select_db('intranet');
-$r=@mysql_query("select user_id, punch_time, punch_date, action from timeclock where user_id = '$id' order by punch_id desc LIMIT 0,1");
-$d=mysql_fetch_array($r, MYSQL_ASSOC);
-return "$d[punch_time] $d[punch_date] ".id2name2($id)." $d[action]<br>";
+	mysql_select_db('intranet');
+	$r=@mysql_query("select user_id, punch_time, punch_date, action from timeclock where user_id = '$id' order by punch_id desc LIMIT 0,1");
+	$d=mysql_fetch_array($r, MYSQL_ASSOC);
+	return "$d[punch_time] $d[punch_date] ".id2name2($id)." $d[action]<br>";
+}
+function getLevel($id){
+	mysql_select_db('core');
+	$r=@mysql_query("select level from ps_users where id = '$id' LIMIT 0,1");
+	$d=mysql_fetch_array($r, MYSQL_ASSOC);
+	return $d[level];
 }
 mysql_select_db('core');
-$r=@mysql_query("SELECT distinct user_id from MDWestServeTimeClock WHERE user_id IN (SELECT id FROM ps_users WHERE level='Operations') order by user_id");
+$r=@mysql_query("SELECT distinct user_id from MDWestServeTimeClock order by user_id");
 while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
-	if ($d[user_id] == 1 || $d[user_id] == 2 || $d[user_id] == 265 || $d[user_id] == 296 || $d[user_id] == 308 || $d[user_id] == 370){
+	if (getLevel($d[user_id]) == 'Operations'){
 		echo "<span style='background-color:yellow;' id='$d[user_id]'>".lastPunch($d[user_id])."</span>";
 	}else{
 		echo "<span id='$d[user_id]'>".lastPunch($d[user_id])."</span>";
